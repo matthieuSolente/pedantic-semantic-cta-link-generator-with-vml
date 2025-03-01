5c41da8af089093ec2e60845d602ca3ef8c75ac6
matthieu solente <yuya1@hotmail.fr>
2025-03-01 13:59:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
 (function($){
- var bgColor,fontBase,fontValue,fontEm,borderwidth,bordervmlwidth,urlLink,btnText,btnClass,cssBtnClass,fontColor,borderColor,fontFamily,bRadius,bordervml,bgcss,bgvml,bordercss,generateCode,generateCss,arcsize,align,msoTextRaise,emTbPadding,emLrPadding,msoFontWidth,htmlMsoTextRaise,fontBold,textTransform,generatePreview;
+ var bgColor,fontBase,fontSize,fontValue,fontEm,borderwidth,bordervmlwidth,urlLink,btnText,btnClass,cssBtnClass,fontColor,borderColor,fontFamily,bRadius,bordervml,bgcss,bgvml,bordercss,generateCode,generateCss,arcsize,align,msoTextRaise,emTbPadding,emLrPadding,tbPadding,lrPadding,vmlwidth,vmlheight,msoFontWidth,htmlMsoTextRaise,fontBold,textTransform,generatePreview;
  
  function updateButton(){ 
     
     urlLink = $('#linkUrl').val();
     btnText = $('#btnText').val();
     if($('#vmlButton').is(':checked')){
+      $('#bodyFont').hide();
       $('#classGroup').show();
       if($.trim($('#btnClass').val()) !=''){
         btnClass = 'class="'+ $('#btnClass').val()+'"'; 
       }else{
            btnClass ='';  
       }
-    }else {
-    $('#classGroup').hide();
-      btnClass =''; 
-    }
-    cssBtnClass = '.' + $('#btnClass').val();
-    fontFamily = $('#fontFamily').val();
-    fontBase = $('#fontBase').val();
-    fontValue = $('#fontSize').val();
-    fontEm = ($('#fontSize').val()/$('#fontBase').val()).toFixed(1)+'em';
-    fontColor = $('#fontColor').val();
-    if($('#btnBorder').is(':checked')){
+      $('#border').show();
+      
+      if($('#btnBorder').is(':checked')){
       $('#borderInput').show();
       borderColor = $('#borderColor').val();
       borderwidth = $('#borderWidth').val();
@@ -35,18 +28,40 @@
       bordervmlwidth = '';
       bordervml = '';
     }
-    align = $('#align').val();
+
+    }else {
+    $('#classGroup').hide();
+      btnClass ='';
+    $('#border').hide();
+    $('#borderInput').hide();
+      bordervml = 'stroked="false"';
+    $('#bodyFont').show();
+
+    }
+    cssBtnClass = '.' + $('#btnClass').val();
+    fontFamily = $('#fontFamily').val();
+    fontSize = $('#fontSize').val()+'px';
+    fontBase = $('#fontBase').val();
+    fontValue = $('#fontSize').val();
+    fontEm = ($('#fontSize').val()/$('#fontBase').val()).toFixed(1)+'em';
+    fontColor = $('#fontColor').val();
+   
     if( $.trim($('#bRadius').val())==''){
     bRadius='';
     } else {
       bRadius = 'border-radius:' + $('#bRadius').val()+'px;';
     }
-     arcsize = Math.ceil($('#bRadius').val() / $(cssBtnClass).outerHeight()*100)+'%';
+    arcsize = Math.ceil($('#bRadius').val() / $(cssBtnClass).outerHeight()*100)+'%';
     bgColor = $('#bgColor').val();
     bgcss = 'background-color:'+ $('#bgColor').val();
     bgvml ='fillcolor="'+ $('#bgColor').val()+'"';
     emTbPadding = ($('#tbPadding').val() / $('#fontBase').val()).toFixed(2)+'em';
     emLrPadding = ($('#lrPadding').val() / $('#fontBase').val()).toFixed(2)+'em';
+    tbPadding = $('#tbPadding').val()+'px';
+    lrPadding = $('#lrPadding').val()+'px';
+    vmlwidth = 'width:' + Math.ceil($(cssBtnClass).outerWidth()).toFixed(1)*0.75+'pt';
+    vmlheight = 'height:' + Math.ceil($(cssBtnClass).outerHeight()).toFixed(1)*0.75+'pt';
+     
     msoFontWidth = Math.ceil(($('#lrPadding').val() / $('#fontBase').val()) / ($('#fontSize').val() / $('#fontBase').val())*100)+'%';
     msoTextRaise = Math.ceil(($('#tbPadding').val() / $('#fontBase').val()) / ($('#fontSize').val() / $('#fontBase').val())*100)*2+'%';
     htmlMsoTextRaise = Math.ceil(($('#tbPadding').val()/$('#fontBase').val()) / ($('#fontSize').val() / $('#fontBase').val())*100)+'%';
@@ -61,11 +76,13 @@
       textTransform = '';
     }
   if($('#vmlButton').is(':checked')){ 
-    generatePreview = '<!--[if mso]>\n<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="mso-position-horizontal:'+align+';v-text-anchor:middle;mso-wrap-style:none" arcsize="'+arcsize+'" '+bordervml+' '+bordervmlwidth+' '+bgvml+'>\n<w:anchorlock/>\n<v:textbox inset="0,0,0,0" style="mso-fit-shape-to-text:true">\n<![endif]-->\n<a '+btnClass+' href="'+urlLink+'" style="'+bordercss+'font-family: '+fontFamily+'; font-size:'+fontEm+'; mso-line-height-rule: exactly;line-height:1.5; background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+' mso-border-alt: none;mso-padding-alt:0px;">\n<!--[if mso]><i style="mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'" hidden>&#8195;</i><span style="mso-text-raise:'+htmlMsoTextRaise+'"><![endif]-->'+btnText+'<!--[if mso]></span><i style="mso-font-width:'+msoFontWidth+';" hidden>&#8195;&#8203;</i><![endif]--></a>\n<!--[if mso]>\n</v:textbox>\n</v:roundrect>\n<![endif]-->';
-    generateCode = '&lt;!--[if mso]&gt;\n&lt;v:roundrect xmlns:v=&quot;urn:schemas-microsoft-com:vml&quot; xmlns:w=&quot;urn:schemas-microsoft-com:office:word&quot; style=&quot;mso-position-horizontal:'+align+';v-text-anchor:middle;mso-wrap-style:none&quot; arcsize=&quot;'+arcsize+'&quot; '+bordervml+' '+bordervmlwidth+' '+bgvml+'&gt;\n&lt;w:anchorlock/&gt;\n&lt;v:textbox inset=&quot;0,0,0,0&quot; style=&quot;mso-fit-shape-to-text:true&quot;&gt;\n&lt;![endif]--&gt;\n&lt;a '+btnClass+' href=&quot;'+urlLink+'&quot; style=&quot;'+bordercss+'font-family: '+fontFamily+'; font-size:'+fontEm+'; mso-line-height-rule: exactly;line-height:1.5; background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+' mso-border-alt: none;mso-padding-alt:0px;&quot;&gt;\n&lt;!--[if mso]&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'&quot; hidden&gt;&amp;#8195;&lt;/i&gt;&lt;span style=&quot;mso-text-raise:'+htmlMsoTextRaise+'&quot;&gt;&lt;![endif]--&gt;'+btnText+'&lt;!--[if mso]&gt;&lt;/span&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';&quot; hidden&gt;&amp;#8195;&amp;#8203;&lt;/i&gt;&lt;![endif]--&gt;&lt;/a&gt;\n&lt;!--[if mso]&gt;\n&lt;/v:textbox&gt;\n&lt;/v:roundrect&gt;\n&lt;![endif]--&gt;'; 
+  
+
+  generatePreview = '<!--[if mso]>\n<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="v-text-anchor:middle;'+vmlheight+';'+vmlwidth+'" arcsize="'+arcsize+'" '+bordervml+' '+bordervmlwidth+' '+bgvml+'>\n<v:textbox inset="0,0,0,0" style="mso-fit-shape-to-text:f">\n<center>\n<![endif]-->\n<a '+btnClass+' href="'+urlLink+'" style="'+bordercss+'font-family:'+fontFamily+';font-size:'+fontSize+';mso-line-height-rule:exactly;line-height:1.5;mso-line-height-alt:150%;background-color:'+ bgColor +';text-decoration:none;text-underline-color:'+bgColor+';'+textTransform+' padding:'+tbPadding+' '+lrPadding+';color:'+fontColor+';'+fontBold+'display:inline-block;'+bRadius+';mso-border-alt:none;mso-padding-alt:0px;">\n'+btnText+'</a>\n<!--[if mso]>\n</center>\n</v:textbox>\n</v:roundrect>\n<![endif]-->';
+    generateCode = '&lt;!--[if mso]&gt;\n&lt;v:roundrect xmlns:v=&quot;urn:schemas-microsoft-com:vml&quot; xmlns:w=&quot;urn:schemas-microsoft-com:office:word&quot; style=&quot;v-text-anchor:middle;'+vmlheight+';'+vmlwidth+'&quot; arcsize=&quot;'+arcsize+'&quot; '+bordervml+' '+bordervmlwidth+' '+bgvml+'&gt;\n&lt;v:textbox inset=&quot;0,0,0,0&quot; style=&quot;mso-fit-shape-to-text:f&quot;&gt;\n&lt;center&gt;\n&lt;![endif]--&gt;\n&lt;a '+btnClass+' href=&quot;'+urlLink+'&quot; style=&quot;'+bordercss+'font-family:'+fontFamily+';font-size:'+fontSize+';mso-line-height-rule:exactly;line-height:1.5;mso-line-height-alt:150%;background-color:'+ bgColor +';text-decoration:none;text-underline-color:'+bgColor+';'+textTransform+' padding:'+tbPadding+' '+lrPadding+';color:'+fontColor+';'+fontBold+'display:inline-block;'+bRadius+';mso-border-alt:none;mso-padding-alt:0px;&quot;&gt;\n'+btnText+'&lt;/a&gt;\n&lt;!--[if mso]&gt;\n&lt;/center&gt;\n&lt;/v:textbox&gt;\n&lt;/v:roundrect&gt;\n&lt;![endif]--&gt;';
   }else{
-    generatePreview = '<a '+btnClass+' href="'+urlLink+'" style="'+bordercss+'font-family: '+fontFamily+'; font-size:'+fontEm+'; mso-line-height-rule: exactly;line-height:1.5; background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+'mso-padding-alt:0px;">\n<!--[if mso]><i style="mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'" hidden>&#8195;</i><span style="mso-text-raise:'+htmlMsoTextRaise+'"><![endif]-->'+btnText+'<!--[if mso]></span><i style="mso-font-width:'+msoFontWidth+';" hidden>&#8195;&#8203;</i><![endif]--></a>';
-    generateCode = '&lt;a '+btnClass+' href=&quot;'+urlLink+'&quot; style=&quot;'+bordercss+'font-family: '+fontFamily+'; font-size:'+fontEm+'; mso-line-height-rule: exactly;line-height:1.5; background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+'mso-padding-alt:0px;&quot;&gt;\n&lt;!--[if mso]&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'&quot; hidden&gt;&amp;#8195;&lt;/i&gt;&lt;span style=&quot;mso-text-raise:'+htmlMsoTextRaise+'&quot;&gt;&lt;![endif]--&gt;'+btnText+'&lt;!--[if mso]&gt;&lt;/span&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';&quot; hidden&gt;&amp;#8195;&amp;#8203;&lt;/i&gt;&lt;![endif]--&gt;&lt;/a&gt;'; 
+    generatePreview = '<a '+btnClass+' href="'+urlLink+'" style="font-family: '+fontFamily+'; font-size:'+fontEm+';background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+'mso-padding-alt:0px;mso-border-alt:none">\n<!--[if mso]><i style="mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'" hidden>&emsp;</i><span style="mso-text-raise:'+htmlMsoTextRaise+'"><![endif]-->'+btnText+'<!--[if mso]></span><i style="mso-font-width:'+msoFontWidth+';" hidden>&emsp;&#8203;</i><![endif]--></a>';
+    generateCode = '&lt;a '+btnClass+' href=&quot;'+urlLink+'&quot; style=&quot;font-family: '+fontFamily+'; font-size:'+fontEm+';background-color:'+ bgColor +';text-decoration: none;text-underline-color:'+bgColor+';'+textTransform+' padding: '+emTbPadding+' '+emLrPadding+'; color:'+fontColor+';'+fontBold+' display:inline-block; '+bRadius+'mso-padding-alt:0px;mso-border-alt:none&quot;&gt;\n&lt;!--[if mso]&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';mso-text-raise:'+msoTextRaise+'&quot; hidden&gt;&amp;emsp;&lt;/i&gt;&lt;span style=&quot;mso-text-raise:'+htmlMsoTextRaise+'&quot;&gt;&lt;![endif]--&gt;'+btnText+'&lt;!--[if mso]&gt;&lt;/span&gt;&lt;i style=&quot;mso-font-width:'+msoFontWidth+';&quot; hidden&gt;&amp;emsp;&amp;#8203;&lt;/i&gt;&lt;![endif]--&gt;&lt;/a&gt;'; 
   } 
   if($('#fontColor').val()==='#ffffff'||$('#fontColor').val()==='#fff'){
     generateCss = '&lt;!--[if gte mso 16]&gt;\n&lt;style&gt;\n'+cssBtnClass+'{\nbackground: transparent !important;\nmso-style-textfill-type:gradient;\n mso-style-textfill-fill-gradientfill-stoplist:&quot;0 \#FFFFFF 0 100000\,100000 \#FFFFFF 0 100000&quot;;\ncolor:#000000 !important;\n background: transparent !important;\n}\n&lt;/style&gt;\n&lt;![endif]--&gt;';
